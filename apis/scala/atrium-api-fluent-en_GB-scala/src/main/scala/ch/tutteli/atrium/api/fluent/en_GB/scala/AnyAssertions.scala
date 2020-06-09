ce7172ebf8fc30c64e7d0b230082ec4cee46ffb2
Robert Stoll <rstoll@tutteli.ch>
2020-06-09 21:09:39 +0200

    for metals with ❤️

--- apis/scala/atrium-api-fluent-en_GB-scala/src/main/scala/ch/tutteli/atrium/api/fluent/en_GB/scala/AnyAssertions.scala
+++ apis/scala/atrium-api-fluent-en_GB-scala/src/main/scala/ch/tutteli/atrium/api/fluent/en_GB/scala/AnyAssertions.scala
@@ -1,8 +1,19 @@
 package ch.tutteli.atrium.api.fluent.en_GB.scala
 
+import ch.tutteli.atrium.assertions.Assertion
 import ch.tutteli.atrium.creating.Expect
 import ch.tutteli.atrium.domain.builders.ExpectImpl
+import ch.tutteli.atrium.domain.builders.creating.AnyAssertionsBuilder
 
 class AnyAssertions[T](expect: Expect[T]) {
-    def toBe(expected: T): Expect[T] = expect.addAssertion(ExpectImpl.INSTANCE.getAny().toBe(expect, expected))
+    def addAssertion(f: AnyAssertionsBuilder => Assertion): Expect[T] =
+        expect.addAssertion(f(ExpectImpl.INSTANCE.getAny()))
+
+    def toBe(expected: T): Expect[T] = addAssertion(_.toBe(expect, expected))
+
+    def notToBe(expected: T): Expect[T] = addAssertion(_.notToBe(expect, expected))
+
+    def isSameAs(expected: T): Expect[T] = addAssertion(_.isSame(expect, expected))
+
+    def isNotSameAs(expected: T): Expect[T] = addAssertion(_.isNotSame(expect, expected))
 }
