f66478c33f68b6c9e7c3ecb233db9366724cfef2
Guga Tkesheladze <guram.tkesheladze@azry.com>
2020-07-13 21:45:22 +0400

    cleaned redundant full path user  of class

--- core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/core/migration/toAtriumLocale.kt
+++ core/api/atrium-core-api-jvm/src/main/kotlin/ch/tutteli/atrium/core/migration/toAtriumLocale.kt
@@ -7,7 +7,7 @@ import ch.tutteli.kbox.blankToNull
 
 @Suppress("DeprecatedCallableAddReplaceWith")
 @Deprecated("Only here to ease migration, use Atrium's Locale directly; will be removed with 1.0.0")
-fun java.util.Locale.toAtriumLocale(): Locale = ch.tutteli.atrium.reporting.translating.Locale(
+fun java.util.Locale.toAtriumLocale(): Locale = Locale(
     language,
     script.blankToNull(),
     country.blankToNull(),
